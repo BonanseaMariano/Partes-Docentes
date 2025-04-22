ec2e15d13eaea840177ddc4385425227e956ffb9
Mariano Bonansea <marianobonanseapetrovial@gmail.com>
2025-04-22 10:22:39 -0300

    Mejorar la ejecución de tests

--- testing/cucumber.js
+++ testing/cucumber.js
@@ -1,3 +1,8 @@
 module.exports = {
-  default: `--format-options '{"snippetInterface": "synchronous"}'`
+  default: [
+    // Asegurarse de que Cucumber encuentre los step definitions en la nueva estructura
+    '--require features/**/*.js',
+    '--require features/step_definitions/**/*.js',
+    `--format-options '{"snippetInterface": "synchronous"}'`
+  ].join(' ')
 }
