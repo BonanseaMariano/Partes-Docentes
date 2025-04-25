301c246637d8912d71b194432963a2d8a8eac372
Mariano Bonansea <marianobonanseapetrovial@gmail.com>
2025-04-25 00:12:19 -0300

    Configuracion correcta del proxy, lista y nueva persona version base

--- frontend/cli/src/app/app.routes.ts
+++ frontend/cli/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PersonasComponent } from './persona/list/personas.component';
+import { PersonaDetailComponent } from './persona/detail/persona-detail.component';
 
 export const routes: Routes = [
     { path: "", component: HomeComponent },
     { path: "personas", component: PersonasComponent },
+    { path: "personas/:dni", component: PersonaDetailComponent },
 ];
