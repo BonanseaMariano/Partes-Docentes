bd4e7eb08967841c9bd876d51ca006c83a317205
Mariano Bonansea <marianobonanseapetrovial@gmail.com>
2025-05-05 23:15:42 -0300

    Modificar la relación OneToMany en la clase Cargo para usar CascadeType.PERSIST y eliminar FetchType.LAZY.

--- backend/src/main/java/unpsjb/labprog/backend/model/Cargo.java
+++ backend/src/main/java/unpsjb/labprog/backend/model/Cargo.java
@@ -9,7 +9,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -121,7 +120,7 @@ public class Cargo {
      * del cargo.
      */
     @NotNull
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @OneToMany(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "cargo_id", nullable = false)
     private List<Horario> horarios = new ArrayList<>();
 }
